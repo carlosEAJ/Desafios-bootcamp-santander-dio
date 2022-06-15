be10e1301b73f895d3480bd9cc21c9aaff736b5c
carlosEAJ <ceajedujpg@gmail.com>
2022-06-15 15:39:04 -0300

    começo do curso de javascript

---
+++ javascript/introdução/teste.js
@@ -0,0 +1,19 @@
+//var y = 0; comentario de linha
+/*var x = 0; comentario de varias linhas*/ 
+
+
+//var preco = 2;
+/*  var desconto = 0.2;
+    const PRECO = 2;
+    var total = PRECO - desconto;
+
+PRECO = PRECO - desconto; */
+
+//parte 2 do teste
+
+function soma(a, b){
+    //console.log(a +b); usado para debug e verificar se a função esta sendo chamada
+    return a + b;
+}
+
+soma(3, 5);
