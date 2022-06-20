fa8edc8753c97a945fc59992968d31d1b0cc132a
carlosEAJ <ceajedujpg@gmail.com>
2022-06-20 16:10:05 -0300

    atividade feita

--- javascript/coleções/atvd1maps.js
+++ javascript/coleções/atvd1maps.js
@@ -1,13 +1,20 @@
 function getAdmins(mapss){
+    let list = []; // let criada para lista e depois verificar os cargos dentro do if
    for(adm of mapss){
-    console.log(adm);
+    if('admin'=== adm[1]){  // (colchetes verifica) verificar se os cargos (indice 1) sao iguais a admin
+        list.push(adm[0]); // se forem iguais adicionar na lista nomes que tem o cargo admin
+    }
    }
+   return list;
 //funcoes atribuidas com nomes genericos, qualquer nome pode ser atribuido
 }
-const myMap = new Map()
-myMap.set('carlos', 'Admin');
-myMap.set('joao', 'admin')
-myMap.set('jurandir','admin')
-myMap.set('lucio','admin')
+const myMap = new Map();
+myMap.set('carlos', 'admin');
+myMap.set('joao', 'usuario');
+myMap.set('jurandir','admin');
+myMap.set('lucio','usuario');
 
-getAdmins(myMap) // chamando a funcao e atribuindo a constante
+const cargo = getAdmins(myMap); //const pegando o valor do retorno da funcao
+// chamando a funcao e atribuindo a constante
+console.log(cargo);
+console.log(myMap);
