3cf0c3282c653e23ac87ae01bac0339143de7e6f
carlosEAJ <ceajedujpg@gmail.com>
2022-06-30 14:58:34 -0300

    modulos

---
+++ javascript/módulos/main.js
@@ -0,0 +1,5 @@
+import * as funcoes from './funcoes.js';
+
+console.log(funcoes.mostraCidade('carlos', 'rj'));
+console.log(funcoes.mostraHobby('carlos', 'Jogos online'));
+console.log(funcoes.mostraIdade('carlos', 21));
